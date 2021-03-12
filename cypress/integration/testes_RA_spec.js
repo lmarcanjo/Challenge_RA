9eb291f363de8151e53d8b5d3b2481fa1dce43a5
lmarcanjo <lmarcanjo16@gmail.com>
2021-03-12 14:12:34 -0300

    Challenge_RA

---
+++ cypress/integration/testes_RA_spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+context('Abrir plataforma', () => {
+   
+    it('Acessar o site RA', () => {
+        cy.visit('https://www.reclameaqui.com.br/')
+           })
+
+     it('Cadastro na plataforma (consumidor)', () => {
+        cy.get('.login-ra__account-text').click()
+        cy.get('.padding-35 > .btn').click()
+        cy.contains('cadastrar').click()
+        cy.get('.col-md-offset-1 > .card > .box-form > .btn').click()
+        
+     }
+     )
+})
